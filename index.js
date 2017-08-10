136cbc7c0a7556699d788273cf402186726e0208
Shraddhaz <shrazing29@gmail.com>
2017-08-09 18:07:17 -0700

    Indentation

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ var fs = require('fs');
 var multer = require('multer');
 var upload = multer({ dest: 'uploads/' });
 var vision = require('@google-cloud/vision');
-
 var request = require('request');
+var app = express();
+
+
 var apiKey = encodeURIComponent('193D8DE7A9');
 var len = encodeURIComponent(2);
 var url = `http://api.smmry.com/&SM_API_KEY=${apiKey}&SM_LENGTH=${len}`;
+
+
 var text;
 var options;
 
-var app = express();
+
 var visionClient = vision({
     projectId: 'swift-adviser-176104',
     keyFilename: 'key.json'
@@ -36,7 +40,11 @@ app.post('/', upload.single('input_image'), function (req,res,next) {
     res.writeHead(200, {
         'Content-Type': 'text/html'
     });
+
+
     var fileName = req.file.path;
+
+
     visionClient.textDetection({source: {filename: fileName}})
         .then(function (responses) {
             const detections = responses[0].textAnnotations;
