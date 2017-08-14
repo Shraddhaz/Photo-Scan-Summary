f28b77f3eb5a7ca8e351b9ccce513046ff0aaf53
Shraddhaz <shrazing29@gmail.com>
2017-08-14 14:36:45 -0700

    Mustache added

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ var upload = multer({ dest: 'uploads/' });
 var vision = require('@google-cloud/vision');
 var request = require('request');
 var bodyParser = require('body-parser');
-var ejs = require('ejs');
-
+var mustache = require('mustache');
 
 var app = express();
 
@@ -27,52 +26,61 @@ var visionClient = vision({
     keyFilename: 'key.json'
 });
 
-fs.readFile('index.html', 'utf-8', function(err, content) {
+fs.readFile('index.html', 'utf-8', function(err, data) {
     if (err) {
         res.end('error occurred');
         return;
     }
-    var compiled = ejs.compile(content);
+    //var compiled = ejs.compile(content);
 
-app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/', upload.single('input_image'), function (req,res,next) {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
+    app.post('/', upload.single('input_image'), function (req,res,next) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
 
 
         var fileName = req.file.path;
 
-    visionClient.textDetection({source: {filename: fileName}})
-        .then(function (responses) {
-            const detections = responses[0].textAnnotations;
-            text = detections[0].description;
-            options = {
-                url: url,
-                method: 'POST',
-                form: {'sm_api_input': text}
-            };
+        visionClient.textDetection({source: {filename: fileName}})
+            .then(function (responses) {
+                const detections = responses[0].textAnnotations;
+                text = detections[0].description;
+                options = {
+                    url: url,
+                    method: 'POST',
+                    form: {'sm_api_input': text}
+                };
 
-            request(options, function (err, response, body) {
-                if (err) {
-                    res.end(error);
-                }
-                else if(JSON.parse(body).sm_api_message == "TEXT IS TOO SHORT"){
-                    res.end(compiled({summ:text}));
-                }
-                else if(JSON.parse(body).sm_api_message =="INSUFFICIENT VARIABLES"){
-                    res.end(compiled({summ:'Insufficient data. Please try again'}));
-                }
-                else {
-                    summry = JSON.parse(body).sm_api_content;
-                    res.end(compiled({summ: summry}));
-                }
+                request(options, function (err, response, body) {
+                    if (err) {
+                        res.send(error);
+                    }
+                    else if(JSON.parse(body).sm_api_message == "TEXT IS TOO SHORT"){
+                        res.write(mustache.render(data.toString(), {
+                            'summ': [
+                                { 'summry': text}]}));
+                    }
+                    else if(JSON.parse(body).sm_api_message == "INSUFFICIENT VARIABLES"){
+                        res.write(mustache.render(data.toString(), {
+                            'summ': [
+                                { 'summry': 'Insufficient data. Please try again'}
+                                ]
+                        }));
+                    }
+                    else {
+                        summry = JSON.parse(body).sm_api_content;
+                        res.write(mustache.render(data.toString(), {
+                            'summ': [
+                                { 'summry': summry }
+                                ]
+                        }));
+                    }
+                });
             });
-            });
-        });
+    });
 });
-
 
 app.listen(8080, function () {
     console.log('Server running at http://127.0.0.1:8080/');
